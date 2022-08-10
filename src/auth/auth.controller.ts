fe7c6421d2fa58ee45ab5ee5e0385803eb4ad76d
Elvis Huges <elvis.huges@blockhub.builders>
2022-08-09 22:23:15 -0300

    🎨 add decorators do swangger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UsersService } from './../users/users.service';
 
-@Controller()
+@ApiTags('auth')
+@Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
@@ -14,9 +16,8 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @ApiOperation({ summary: 'Login' })
   async login(@Body() loginDto: UserLoginDto, @Request() req): Promise<any> {
-    console.log('loginDto', loginDto);
-
     return req.user;
   }
 }
